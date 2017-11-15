28e158e97fe95a10afd9332c67a20d88f449a6ed
Purusharth Singh Tomar <ptomar@usc.edu>
2017-11-14 23:17:58 -0800

    Level00.json + LoadLevel + Updated Size to 40x40

---
+++ PhaserGame/Assets/README.md
@@ -0,0 +1,12 @@
+Instructions for level design -
+
+File naming convention: Level0x.json (x is from 0 to 9 inclusive).
+Level00.json has already been created. Please copy it and edit that for other levels.
+Each block is 40x40 and so is the ball.
+The game screen is 800x600.
+Therefore, in each row there can be max 20 blocks.
+There are 15 rows in total.
+A, B, C, D, E and F represent BlockA through BlockF.
+P represents Ball Start Position.
+Q represents Ball Finish Position.
+***Make sure to leave first row as ......... because we are displaying time in that row.
--- PhaserGame/main.js
+++ PhaserGame/main.js
@@ -8,23 +8,93 @@ var leftButton;
 var upButton;
 var downButton;
 var mBall;
+var mFinishBlock;
+var levelData;
+var mBlocks = []; //an array of all the blocks
+const WIDTH = 40;
+const HEIGHT = 40;
+
+
 function preload(){
 	game.stage.backgroundColor = "#000000";
-	game.load.image('mBall', "assets/ball.png");
-	game.load.image('mBlock1', "assets/block.png");
+	game.load.image('Ball', "Assets/Ball.png");
+	game.load.image('BlockA', "Assets/BlockA.png");
+	game.load.image('BlockB', "Assets/BlockB.png");
+	game.load.image('BlockC', "Assets/BlockC.png");
+	game.load.image('BlockD', "Assets/BlockD.png");
+	game.load.image('BlockE', "Assets/BlockE.png");
+	game.load.image('BlockF', "Assets/BlockF.png");
+	//game.load.image('Finish', "Assets/Finish.png"); - TOOO
+	
+	//Load Level
+	game.load.json('level', 'Assets/Level00.json');
+}
+
+function LoadLevel() {
+    levelData = game.cache.getJSON('level');
+    for(var i = 0; i < levelData.levelRows.length; i++)
+    {
+    	var rowString = levelData.levelRows[i];
+    	for(var j = 0; j < rowString.length; j++){
+    		if(rowString[j] == 'A')
+    		{
+    			var newBlock = game.add.sprite(j*WIDTH, i*HEIGHT , 'BlockA');
+    			mBlocks.push(newBlock);
+    		}
+    		else if(rowString[j] == 'B')
+    		{
+    			var newBlock = game.add.sprite(j*WIDTH, i*HEIGHT , 'BlockB');
+    			mBlocks.push(newBlock);
+    		}
+    		else if(rowString[j] == 'C')
+    		{
+    			var newBlock = game.add.sprite(j*WIDTH, i*HEIGHT , 'BlockC');
+    			mBlocks.push(newBlock);
+    		}
+    		else if(rowString[j] == 'D')
+    		{
+    			var newBlock = game.add.sprite(j*WIDTH, i*HEIGHT , 'BlockD');
+    			mBlocks.push(newBlock);
+    		}
+    		else if(rowString[j] == 'E')
+    		{
+    			var newBlock = game.add.sprite(j*WIDTH, i*HEIGHT , 'BlockE');
+    			mBlocks.push(newBlock);
+    		}
+    		else if(rowString[j] == 'F')
+    		{
+    			var newBlock = game.add.sprite(j*WIDTH, i*HEIGHT , 'BlockF');
+    			mBlocks.push(newBlock);
+    		}
+    		else if(rowString[j] == 'P')
+    		{
+    			mBall = game.add.sprite(j*WIDTH, i*HEIGHT , 'Ball');
+    		}
+    		else if(rowString[j] == 'Q')
+    		{
+    			//**Code for finish block
+    			//mFinishBlock = game.add.sprite(j*WIDTH, i*HEIGHT , 'Finish');
+    			//mBlocks.push(mFinishBlock);
+    		}
+    	}
+    }
 }
 
 function create(){
+	
+	//LoadLevel
+	LoadLevel();
+	
 	//Time
-	timeLeftText = game.add.text(32, 32, 'Time Left: ', { font: "24px Century Gothic", fill: "#ffffff", align: "left" });
-	timeText = game.add.text(150, 32, '', { font: "24px Century Gothic", fill: "#00ff00", align: "left" });
+	timeLeftText = game.add.text(20, 10, 'Time Left: ', { font: "24px Century Gothic", fill: "#ffffff", align: "left" });
+	timeText = game.add.text(140, 10, '', { font: "24px Century Gothic", fill: "#00ff00", align: "left" });
 	game.time.events.add(Phaser.Timer.SECOND * TOTAL_TIME, endGame, this);
+	game.world.bringToTop(timeLeftText);
+	game.world.bringToTop(timeText);
 	
 	//Ball physics
 	game.physics.startSystem(Phaser.Physics.ARCADE);
-	mBall = game.add.sprite(game.world.centerX, 500, 'mBall');
-	mBall.scale.setTo(0.2, 0.2);
-	mBall.anchor.set(0.5);
+	//mBall.anchor.set(0.5); Don't need anchor - collision would be easier without them
 	mBall.enablebody = true;
 	game.physics.enable(mBall, Phaser.Physics.ARCADE);
     mBall.checkWorldBounds = true;
@@ -32,6 +102,7 @@ function create(){
     mBall.body.bounce.set(1);
     mBall.body.velocity.x = 5;
     mBall.body.velocity.y = 5;
+    
 	rightButton = this.input.keyboard.addKey(Phaser.KeyCode.D);
 	leftButton = this.input.keyboard.addKey(Phaser.KeyCode.A);
 	upButton = this.input.keyboard.addKey(Phaser.KeyCode.W);
@@ -39,11 +110,13 @@ function create(){
 }
 
 function update(){
+	
 	if(game.time.events.duration/1000 <= 10) //time left = game.time.events.duration/1000
 	{
 		timeText.fill = "#ff0000";
 	}
 	timeText.setText("" + parseInt(game.time.events.duration/1000));
+	
 	if(downButton.isDown && !upButton.isDown){
 		mBall.body.velocity.y += 5;
 	}
@@ -60,6 +133,7 @@ function update(){
 
 	mBall.body.velocity.x *= 0.999;
 	mBall.body.velocity.y *= 0.999;
+	
 }
 
 function render(){
